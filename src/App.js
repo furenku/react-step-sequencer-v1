39d9b81a5f27a42a62ea93d9e5837e8a7a813cd1
furenku <furenku@gmail.com>
2018-06-18 22:32:28 -0500

    stepsequencer playable with keyboard keys

--- src/App.js
+++ src/App.js
@@ -1,122 +1,80 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import kick from './samples/kick.wav';
 import snare from './samples/snare.wav';
 import hihat from './samples/hihat.ogg';
 
-const SamplePlayer = React.forwardRef((props,ref) => {
-  let audio;
-  return (
-    <audio ref={ref} src={props.src} controls/>
-  )
 
+const SamplePlayer = React.forwardRef((props, ref) => {
+  return (
+    <audio src={props.src} ref={ref} controls/>
+  )
 })
 
 
 class App extends Component {
 
-  
-  samples = []
-  
-  constructor(props) {
-    super(props);
-    let n = 16;
-    this.beats = Array.apply(null, {length: n}).map(Number.call, Number)
+  samples = [
+    kick,
+    snare,
+    hihat
+  ];
 
-    console.log(this.beats);
-
-    this.samplers = [
-      React.createRef(),
-      React.createRef(),
-      React.createRef(),
-    ]
-
-    this.samples[0] = kick;
-    this.samples[1] = snare;
-    this.samples[2] = hihat;
-    
-  }
-  
-
-  componentDidMount() {
-    // this.play()    
-  }
-
-  componentWillUnmount() {
-
-    this.stop();
-  
-  }
-
+  samplePlayerRefs = [];
 
   handleKeyPress = (event) => {
-    if(event.key == 'a'){
-      this.trigger(this.samplers[0].current)
+    
+    const indexes = {
+      'a': 0,
+      'b': 1,
+      'c': 2,
     }
-    if(event.key == 'b'){
-      this.trigger(this.samplers[1].current)
+
+    let index = indexes[event.key];
+    
+    if( index != undefined ) {
+      
+      let audio = this.samplePlayerRefs[index].current;
+      
+      this.trigger( audio )
+      
     }
-    if(event.key == 'c'){
-      this.trigger(this.samplers[2].current)
-    }
-  }
- 
+ }
+
   trigger = audio => {
-    audio.currentTime = 0
-    audio.play()
-  }
-
-  play() {
-    let i = 0;
-    
-    if( ! this.timer ) {
-
-      this.timer = setInterval( () => {
-        this.samplers[i].current.play();
-        i++;
-        i%=this.samplers.length;
-      }, 500)
-
-    }
-
-  }
-
-  stop() {
-    
-    clearInterval( this.timer );
-    this.timer = null
-
-  }
-
-  clickBeatButton( i ) {
-    console.log(i);
-    
+    audio.currentTime = 0;
+    audio.play();
   }
 
   render() {
+
     let samplePlayers = this.samples.map( ( sample, i ) => {
       
-      return <SamplePlayer key={i} ref={this.samplers[i]} src={this.samples[i]}/>
-        
+      this.samplePlayerRefs[i] = React.createRef()
+      
+      return <SamplePlayer
+        key={i}
+        src={sample}
+        ref={this.samplePlayerRefs[i]}
+      />
+
     })
 
-
-    let beatButtons = this.beats.map( n => <button onClick={ () => this.clickBeatButton(n) }>{n}</button>)
+    console.log( samplePlayers )
 
     return (
       <div>
+        
         {samplePlayers}
-        <input type="text" id="one" onKeyPress={this.handleKeyPress} />
-        <button onClick={()=>this.play()}>Play</button>
-        <button onClick={()=>this.stop()}>Stop</button>
+        
+        <input type="text" onKeyPress={this.handleKeyPress} />
 
-        <div>
-          {beatButtons}
-        </div>
       </div>
     );
   }
 }
 
 export default App;
+
